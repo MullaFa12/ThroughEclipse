733450f1bfb4d46db6804e334314da4c38ae9bdf
Fatima <fatima@gmail.com>
2024-05-18 17:13:25 +0530

    through eclipse [Fatima]

---
+++ src/test/java/manualPush/Class2.java
@@ -0,0 +1,10 @@
+package manualPush;
+
+public class Class2 {
+	
+	public void Test1() {
+		System.out.println("Class1");
+
+	}
+
+}
---
+++ src/test/java/manualPush/Class3.java
@@ -0,0 +1,10 @@
+package manualPush;
+
+public class Class3 {
+	
+	public void Test1() {
+		System.out.println("Class1");
+
+	}
+
+}
